51f3d8ca016a65597cfa6ce2c0509e1837016267
[shahbaj] <[shahbaj@froiden.com]>
2018-05-18 18:06:09 +0530

    Feat: register and login component added

---
+++ src/components/custom/carousel.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <carousel :navigationEnabled="true">
+            <slide v-for="(i,index) in items" :key="index">
+                <img :src="i.url" height="100px" width ="100px"/>
+            </slide>
+        </carousel>
+    </div>
+</template>
+<script>
+import { Carousel, Slide } from 'vue-carousel'
+export default{
+  name: 'CustomCarousel',
+  props: ['items'],
+  components: {
+    Carousel,
+    Slide}
+}
+</script>
